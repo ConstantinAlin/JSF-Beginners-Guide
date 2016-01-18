1cc2c4b6bd7cb194611505fa1005185b9b5f70ff
Constantin Alin <constantin_alin_marian@yahoo.com>
2016-01-18 17:07:32 +0200

    JSF h:inputTextarea tutorial

---
+++ hinputTextareaExamples/src/main/java/beans/PlayerBean.java
@@ -0,0 +1,24 @@
+package beans;
+
+import java.io.Serializable;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+@Named
+@SessionScoped
+public class PlayerBean implements Serializable {
+
+    private String text;
+
+    public PlayerBean() {
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+}
