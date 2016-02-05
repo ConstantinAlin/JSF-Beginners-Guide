9ed31b06b9670e32a57f6d877946a09f03aa56fc
Constantin Alin <constantin_alin_marian@yahoo.com>
2016-02-05 17:59:05 +0200

    Passing extra parameters to a custom validator

---
+++ fattributeExamples/fattributeExample_2/src/main/java/beans/GameValidator.java
@@ -0,0 +1,19 @@
+package beans;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.FacesValidator;
+import javax.faces.validator.Validator;
+import javax.faces.validator.ValidatorException;
+
+@FacesValidator
+public class GameValidator implements Validator {
+
+    @Override
+    public void validate(FacesContext context, UIComponent component, Object value) throws ValidatorException {
+        String player = value.toString().replaceAll("\\s+", "");
+        throw new ValidatorException(new FacesMessage("Response: " + component.getAttributes().get("msgAttr" + player)));
+    }
+
+}
